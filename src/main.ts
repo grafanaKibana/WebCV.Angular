e1d35ae0ffb645001febc537af3fee4bfcfd30c2
nikitareshetnik <reshetnik.nikita@gmail.com>
2026-01-11 14:19:05 +0200

    Add `.npmrc` with legacy peer dependency resolution; update `package-lock.json` to include new dependencies `@vercel/analytics` and `@vercel/speed-insights`, and clean up obsolete dependencies.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+
 if (environment.production) {
   enableProdMode();
+  inject();
 }
+injectSpeedInsights();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
