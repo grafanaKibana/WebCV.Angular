e948089b656bfc67a6affddfb8b1a76209b0e92e
nikitareshetnik <reshetnik.nikita@gmail.com>
2026-01-23 16:34:22 +0200

    feat(02-04): block background scroll while dialog open
    - Inject CDK ScrollStrategyOptions in portfolio item
    - Apply block() scrollStrategy to project details MatDialog

--- src/app/pages/portfolio/portfolio-item/portfolio-item.component.ts
+++ src/app/pages/portfolio/portfolio-item/portfolio-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ProjectModel } from '../interfaces/projectModel';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ScrollStrategyOptions } from '@angular/cdk/overlay';
 import { ModalDialogComponent } from '../../../shared/modal-dialog/modal-dialog.component';
 
 @Component({
@@ -11,13 +12,14 @@ import { ModalDialogComponent } from '../../../shared/modal-dialog/modal-dialog.
 export class PortfolioItemComponent {
   @Input() project!: ProjectModel;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private sso: ScrollStrategyOptions) { }
 
   openProjectDetails(project: ProjectModel) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.data = project;
     dialogConfig.panelClass = 'dialog';
     dialogConfig.maxWidth = '';
+    dialogConfig.scrollStrategy = this.sso.block();
     this.dialog.open(ModalDialogComponent, dialogConfig);
   }
 }
