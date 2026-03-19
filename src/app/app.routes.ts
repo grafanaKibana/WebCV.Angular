bbbf8bdfa18a29d921f6fa92a203e379b45f7cd7
nikitareshetnik <reshetnik.nikita@gmail.com>
2026-03-19 02:14:06 +0200

    perf(routes): lazy-load blog routes for smaller initial bundle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,23 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home/home-page/home-page.component';
-import { BlogPageComponent } from './pages/blog/blog-page/blog-page.component';
-import { BlogDetailPageComponent } from './pages/blog/blog-detail-page/blog-detail-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent, data: { animation: 'HomePage' } },
-  { path: 'blog/:slug', component: BlogDetailPageComponent, data: { animation: 'BlogDetailPage' } },
-  { path: 'blog', component: BlogPageComponent, data: { animation: 'BlogPage' }, pathMatch: 'full' }
+  {
+    path: 'blog/:slug',
+    loadComponent: () =>
+      import('./pages/blog/blog-detail-page/blog-detail-page.component').then(
+        (m) => m.BlogDetailPageComponent
+      ),
+    data: { animation: 'BlogDetailPage' }
+  },
+  {
+    path: 'blog',
+    loadComponent: () =>
+      import('./pages/blog/blog-page/blog-page.component').then(
+        (m) => m.BlogPageComponent
+      ),
+    data: { animation: 'BlogPage' },
+    pathMatch: 'full'
+  }
 ];
